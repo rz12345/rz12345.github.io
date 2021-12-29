22279435c76a83600f46064e2ec3844b22d40a03
Rz Chen <rz12345@gmail.com>
2021-12-29 16:41:17 +0800

    加入：美股回測與近場價

---
+++ invest/js/app.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        TargetPrices:[],
+        BackTradeRecords:[],
+        Select:{
+            ItemName: 'BackTradeRecords'
+
+        },
+        FetchComplete:false,
+        NoticeMsg:'',
+    },
+    methods: {
+
+    },
+    mounted: function(){
+        // item & realm data initial 
+        axios.get(`https://invest-8738f-default-rtdb.firebaseio.com/us-stock/bt.json`).then(function(res){
+            this.BackTradeRecords = res.data.sort( (a,b) => b.irr - a.irr);
+        }.bind(this));        
+        axios.get(`https://invest-8738f-default-rtdb.firebaseio.com/us-stock/target_prices.json`).then(function(res){
+            this.TargetPrices = res.data.sort((a,b)=>a.last - a.target_price);
+        }.bind(this));
+        
+    },
+    computed: {
+
+    },
+  });
