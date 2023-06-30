debf4424e85f1c37aaa84b860e1dad9492e5df3e
Rz Chen <rz12345@gmail.com>
2023-06-30 22:47:30 +0800

    更新資料來源

--- invest-us/js/app.js
+++ invest-us/js/app.js
@@ -13,7 +13,7 @@ var app = new Vue({
     },
     methods: {
         getStockLogs: function(stock_code) {            
-            axios.get(`https://invest-8738f-default-rtdb.firebaseio.com/us-stock/logs/${stock_code}.json`).then(function(res){
+            axios.get(`https://jojocat-poc-default-rtdb.firebaseio.com/us-stock/logs/${stock_code}.json`).then(function(res){
                 Vue.set(this.StockLogs, stock_code, res.data);                
             }.bind(this)); 
         }
@@ -26,10 +26,10 @@ var app = new Vue({
     },
     mounted: function(){
         // item & realm data initial 
-        axios.get(`https://invest-8738f-default-rtdb.firebaseio.com/us-stock/bt.json`).then(function(res){
+        axios.get(`https://jojocat-poc-default-rtdb.firebaseio.com/us-stock/bt.json`).then(function(res){
             this.BackTradeRecords = res.data.sort( (a,b) => b.irr - a.irr);
         }.bind(this));        
-        axios.get(`https://invest-8738f-default-rtdb.firebaseio.com/us-stock/target_prices.json`).then(function(res){
+        axios.get(`https://jojocat-poc-default-rtdb.firebaseio.com/us-stock/target_prices.json`).then(function(res){
             this.TargetPrices = res.data.sort((a,b)=>(a.last - a.target_price)/a.last - (b.last - b.target_price)/b.last);
         }.bind(this));
         
